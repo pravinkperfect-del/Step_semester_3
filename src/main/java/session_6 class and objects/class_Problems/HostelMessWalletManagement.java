bfa7bfdf12c8200e6e00d0b3334de8ba8f0c6fee
Pravin <pravinkperfect@gmail.com>
2026-09-20 20:10:37 +0530

    session6 class and objects

---
+++ src/main/java/session_6 class and objects/class_Problems/HostelMessWalletManagement.java
@@ -0,0 +1,45 @@
+class MessWallet {
+    private double balance;
+
+    MessWallet(double balance) {
+        if (balance < 0) {
+            System.out.println("Negative balance not allowed");
+            this.balance = 0;
+        } else {
+            this.balance = balance;
+        }
+    }
+
+    void topUp(double amount) {
+        if (amount <= 0) {
+            System.out.println("Top-up rejected");
+        } else {
+            balance += amount;
+            System.out.println("Balance after top-up: " + balance);
+        }
+    }
+
+    void deduct(double amount) {
+        if (amount > balance) {
+            System.out.println("Deduct rejected: insufficient balance");
+        } else {
+            balance -= amount;
+        }
+    }
+
+    double getBalance() {
+        return balance;
+    }
+}
+
+public class HostelMessWalletManagement {
+    public static void main(String[] args) {
+
+        MessWallet wallet = new MessWallet(500);
+
+        wallet.topUp(200);
+        wallet.deduct(1000);
+
+        System.out.println("Final balance: " + wallet.getBalance());
+    }
+}
